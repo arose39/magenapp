9c7ca35f01d0a56a66f7b40678aecc40e7b55429
Andrii Palamarchuk <a.palamarchuk@elogic.com.ua>
2022-11-18 11:06:17 +0200

    made luxury taxes listing works

---
+++ app/code/Palamarchuk/LuxuryTax/Ui/Component/Listing/Columns/LuxuryTaxActions.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Palamarchuk\LuxuryTax\Ui\Component\Listing\Columns;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class LuxuryTaxActions extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface   $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface       $urlBuilder,
+        array              $components = [],
+        array              $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource): array
+    {
+        if (isset($dataSource['data']['items'])) {
+            $storeId = $this->context->getFilterParam('luxury_tax_id');
+
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'luxurytax/luxurytax/edit',
+                        ['id' => $item['luxury_tax_id']]
+                    ),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+                $item[$this->getData('name')]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'luxurytax/luxurytax/delete',
+                        ['id' => $item['luxury_tax_id']]
+                    ),
+                    'label' => __('Delete'),
+                    'hidden' => false,
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
