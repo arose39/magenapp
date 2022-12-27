a7bea628a79c105eee178305aa6dee561e80e902
Andrii Palamarchuk <a.palamarchuk@elogic.com.ua>
2022-12-27 15:39:29 +0200

    added validation for file and saving functionality for import

--- app/code/Palamarchuk/CatalogCategoriesImportExport/Model/Import/Category.php
+++ app/code/Palamarchuk/CatalogCategoriesImportExport/Model/Import/Category.php
@@ -3,50 +3,59 @@
 namespace Palamarchuk\CatalogCategoriesImportExport\Model\Import;
 
 use Magento\Catalog\Api\CategoryListInterface;
-use Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Catalog\Model\ResourceModel\Category as CategoryResource;
+use Magento\Catalog\Model\ResourceModel\Category\Collection;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Eav\Model\Config;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Model\ResourceModel\PredefinedId;
+use Magento\Framework\Stdlib\StringUtils;
+use Magento\ImportExport\Model\Export\Factory;
 use Magento\ImportExport\Model\Import;
 use Magento\ImportExport\Model\Import\Entity\AbstractEav;
 use Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingErrorAggregatorInterface;
+use Magento\ImportExport\Model\ImportFactory;
+use Magento\ImportExport\Model\ResourceModel\Helper;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Category extends AbstractEav
 {
-    private \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository;
-    private \Magento\Catalog\Model\ResourceModel\Category\Collection $categoryCollection;
-    private \Magento\Catalog\Model\CategoryFactory $categoryFactory;
-
-    /**
-     * @var JoinProcessorInterface
-     */
-    private $extensionAttributesJoinProcessor;
-    protected $masterAttributeCode = 'name';
     public const XML_PATH_BUNCH_SIZE = 'import/format_v2/category_bunch_size';
-    private \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory;
+    public const ERROR_PATH_LEVEL_VALUE = 'pathDoesNotCorrespondLevel';
+    public const ERROR_PATH_VALUE = 'wrongPathValue';
+    protected array $useConfigFields = ['available_sort_by', 'default_sort_by', 'filter_price_range'];
+    protected $masterAttributeCode = 'name';
+    protected CategoryResource $categoryResource;
     private CategoryListInterface $categoryList;
-    private \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria;
-    private $lastCategoryId;
+    private SearchCriteriaInterface $searchCriteria;
+    private int $lastCategoryId;
+    private CategoryRepositoryInterface $categoryRepository;
+    private Collection $categoryCollection;
+    private CategoryFactory $categoryFactory;
 
 
     public function __construct(
-        \Magento\Framework\Stdlib\StringUtils                              $string,
-        \Magento\Framework\App\Config\ScopeConfigInterface                 $scopeConfig,
-        \Magento\ImportExport\Model\ImportFactory                          $importFactory,
-        \Magento\ImportExport\Model\ResourceModel\Helper                   $resourceHelper,
-        \Magento\Framework\App\ResourceConnection                          $resource,
-        ProcessingErrorAggregatorInterface                                 $errorAggregator,
-        \Magento\Store\Model\StoreManagerInterface                         $storeManager,
-        \Magento\ImportExport\Model\Export\Factory                         $collectionFactory,
-        \Magento\Eav\Model\Config                                          $eavConfig,
-        \Magento\Catalog\Api\CategoryRepositoryInterface                   $categoryRepository,
-        \Magento\Catalog\Model\ResourceModel\Category\Collection           $categoryCollection,
-        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory    $categoryCollectionFactory,
-        \Magento\Catalog\Model\ResourceModel\Category\Attribute\Collection $attributeCollection,
-        \Magento\Catalog\Api\CategoryListInterface                         $categoryList,
-        \Magento\Catalog\Model\CategoryFactory                             $categoryFactory,
-        \Magento\Framework\Api\SearchCriteriaInterface                     $searchCriteria,
-        JoinProcessorInterface                                             $extensionAttributesJoinProcessor,
-        array                                                              $data = []
-    )
-    {
+        StringUtils                        $string,
+        ScopeConfigInterface               $scopeConfig,
+        ImportFactory                      $importFactory,
+        Helper                             $resourceHelper,
+        ResourceConnection                 $resource,
+        ProcessingErrorAggregatorInterface $errorAggregator,
+        StoreManagerInterface              $storeManager,
+        Factory                            $collectionFactory,
+        Config                             $eavConfig,
+        CategoryRepositoryInterface        $categoryRepository,
+        Collection                         $categoryCollection,
+        CategoryListInterface              $categoryList,
+        CategoryFactory                    $categoryFactory,
+        SearchCriteriaInterface            $searchCriteria,
+        CategoryResource                   $categoryResource,
+        array                              $data = []
+    ) {
         parent::__construct(
             $string,
             $scopeConfig,
@@ -63,16 +72,82 @@ class Category extends AbstractEav
         $this->categoryList = $categoryList;
         $this->categoryCollection = $categoryCollection;
         $this->categoryFactory = $categoryFactory;
-        $this->extensionAttributesJoinProcessor = $extensionAttributesJoinProcessor;
-        $this->categoryCollectionFactory = $categoryCollectionFactory;
-        $this->attributeCollection = $attributeCollection;
         $this->searchCriteria = $searchCriteria;
         $this->lastCategoryId = (int)$categoryCollection->getLastItem()->getId();
+        $this->categoryResource = $categoryResource;
+
+        $this->addMessageTemplate(
+            self::ERROR_PATH_LEVEL_VALUE,
+            'Value for PATH attribute in row with category name \'%s\' contains incorrect path value nesting should correspond with level value'
+        );
+        $this->addMessageTemplate(
+            self::ERROR_PATH_VALUE,
+            'Value for PATH attribute in row with category name \'%s\'  incorrect. Some Ids from path not found nor in file not in existing categories.
+            Search was provided by level of nesting'
+        );
     }
+
+    use PredefinedId;
 
     public function getEntityTypeCode()
     {
         return 'catalog_category';
+    }
+
+    public function validateData()
+    {
+        parent::validateData();
+        if ($this->getErrorAggregator()->getErrorsCount()) {
+            return $this->getErrorAggregator();
+        }
+        $data = [];
+        while ($bunch = $this->_dataSourceModel->getNextBunch()) {
+            foreach ($bunch as $rowNumber => $rowData) {
+                $data[$rowNumber] = $rowData;
+            }
+        }
+
+        $newLeveledIds = [];
+        foreach ($data as $rowNumber => $rowData) {
+            $explodedPath = explode('/', $rowData['path']);
+            if (isset($explodedPath[$rowData['level']]) && ($explodedPath[$rowData['level']] === end($explodedPath))) {
+                $newLeveledIds[$rowData['level']][] = explode('/', $rowData['path'])[$rowData['level']];
+            } else {
+                $this->addErrors(self::ERROR_PATH_LEVEL_VALUE, [$rowData['name']]);
+            }
+        }
+        $existingLeveledIds = [];
+        foreach ($this->categoryCollection as $category) {
+            $existingLeveledIds[$category->getLevel()][] = $category->getId();
+        }
+        foreach ($data as $rowNumber => $rowData) {
+            $explodedPath = explode('/', $rowData['path']);
+            $pathIsCorrectCounter = 0;
+            foreach ($explodedPath as $level => $id) {
+                $findInNewLeveledIds = false;
+                foreach ($newLeveledIds[$level] as $idFromList) {
+                    if ($id === $idFromList) {
+                        $pathIsCorrectCounter++;
+                        $findInNewLeveledIds = true;
+                        break;
+                    }
+                }
+                if (!$findInNewLeveledIds){
+                    foreach ($existingLeveledIds[$level] as $idFromList) {
+                        if ($id === $idFromList) {
+                            $pathIsCorrectCounter++;
+                            break;
+                        }
+                    }
+                }
+            }
+            if ($pathIsCorrectCounter !== count($explodedPath)){
+                $this->addErrors(self::ERROR_PATH_VALUE, [$rowData['name']]);
+            }
+        }
+
+
+        return $this->getErrorAggregator();
     }
 
     public function validateRow(array $rowData, $rowNumber)
@@ -89,23 +164,19 @@ class Category extends AbstractEav
             case Import::BEHAVIOR_ADD_UPDATE:
                 $this->addAndUpdateEntities();
                 break;
-            case Import::BEHAVIOR_REPLACE:
-                $this->replaceAndAddEntities();
-                break;
         }
-        $this->indexerProcessor->markIndexerAsInvalid();
+//        $this->indexerProcessor->markIndexerAsInvalid();
         return true;
     }
 
 
     private function addAndUpdateEntities()
     {
-        $dividedEntities = $this->divideEntitiesToUpdatedAndCreated();
+        $dividedEntities = $this->getListsOfUpdatedAndCreatedEntities();
         $entitiesToCreate = $dividedEntities['entities_to_create'];
         $entitiesToUpdate = $dividedEntities['entities_to_update'];
         $allEntities = array_merge($entitiesToCreate, $entitiesToUpdate);
 
-        //
 
         // Code below sets new IDs and updates the hierarchy
         /** @var \Magento\Catalog\Model\Category $newEntity */
@@ -146,14 +217,29 @@ class Category extends AbstractEav
             }
         }
 
+        foreach ($entitiesToCreate as $entity) {
+            $entityArray = $entity->toArray();
+            $coorectArrayEntity = [
+                'entity_id' => $entityArray['entity_id'],
+                'attribute_set_id' => 3,
+                'parent_id' => $entityArray['parent_id'],
+                'path' => $entityArray['path'],
+                'position' => $entityArray['position'],
+                'level' => $entityArray['level']
 
-        $I = $entitiesToCreate;
-        // get last ID of entities
-        // add new pathes , id, and parent ids to new entity
-        //save to db
+            ];
+            $this->categoryResource->getConnection()->insertOnDuplicate('catalog_category_entity', $coorectArrayEntity);
+
+            $this->categoryRepository->save($entity);
+        }
+        foreach ($entitiesToUpdate as $entity) {
+            $this->categoryRepository->save($entity);
+        }
+
+        return true;
     }
 
-    protected function divideEntitiesToUpdatedAndCreated()
+    protected function getListsOfUpdatedAndCreatedEntities()
     {
         $entitiesToCreate = [];
         $entitiesToUpdate = [];
@@ -163,22 +249,33 @@ class Category extends AbstractEav
                 $updateFlag = false;
                 foreach ($entityCollection as $entity) {
                     $entityName = $entity->getData('name');
-                    if (!$this->validateRow($rowData, $rowNumber)) {
-                        continue;
-                    }
-                    if ($this->getErrorAggregator()->hasToBeTerminated()) {
-                        $this->getErrorAggregator()->addRowToSkip($rowNumber);
-                        continue;
-                    }
+                    $entityId = $entity->getData('entity_id');
+                    $entityPath = $entity->getData('path');
+
+//                    if (!$this->validateRow($rowData, $rowNumber)) {
+//                        continue;
+//                    }
+//                    if ($this->getErrorAggregator()->hasToBeTerminated()) {
+//                        $this->getErrorAggregator()->addRowToSkip($rowNumber);
+//                        continue;
+//                    }
 
                     if ($entityName === $rowData['name']) {
                         // todo validate if exist in array such fields
-                        $path = $rowData['path'];
-                        unset($rowData['children_count'], $rowData['path'], $rowData['name']);
+                        $newPath = $rowData['path'];
+                        $isActive = ($rowData['is_active'] === "Yes") ? true : false;
+                        $includeInMenu = ($rowData['include_in_menu'] === "Yes") ? true : false;
+                        $isAnchor = ($rowData['is_anchor'] === "Yes") ? true : false;
+                        unset($rowData['is_active'], $rowData['include_in_menu'], $rowData['is_anchor']);
                         $entity->setData($rowData);
-                        if ($entity->getPath() !== $path) {
-                            $entity->setPath($path);
-                            $explodedPath = explode('/', $path ?? '');
+                        $entity->setId($entityId);
+                        $entity->setIsActive($isActive);
+                        $entity->setIncludeInMenu($includeInMenu);
+                        $entity->setIsAnchor($isAnchor);
+                        if ($entityPath !== $newPath) {
+                            $entity->setPath($newPath);
+
+                            $explodedPath = explode('/', $newPath ?? '');
                             if (is_array($explodedPath) && (count($explodedPath) > 1)) {
                                 $newEntityParentId = (int)$explodedPath[($rowData['level'] - 1)];
                                 $entity->setParentId($newEntityParentId);
@@ -204,11 +301,6 @@ class Category extends AbstractEav
         }
 
         return ['entities_to_create' => $entitiesToCreate, 'entities_to_update' => $entitiesToUpdate];
-    }
-
-    private function replaceAndAddEntities()
-    {
-        return true;
     }
 
 
