cdf13e26560c6028ef9713258f23cccbb3c42d56
Andrii Palamarchuk <a.palamarchuk@elogic.com.ua>
2023-02-17 15:22:37 +0200

    added delete hook logic

--- app/code/Palamarchuk/TgOrderNotificationBot/Controller/Adminhtml/Bot/Settings.php
+++ app/code/Palamarchuk/TgOrderNotificationBot/Controller/Adminhtml/Bot/Settings.php
@@ -19,38 +19,19 @@ class Settings extends Action
         private WriterInterface $configWriter,
         private Manager         $cacheManager,
         private Client          $client
-    )
-    {
+    ) {
         parent::__construct($context);
     }
 
     public function execute(): ResultInterface
     {
-
-
-        // https://api.telegram.org/bot5882390745:AAF-lmwKIXukQMjIZhk_DeZ7V_UttQDurH4/setWebhook?url=https://magenapp.requestcatcher.com
-//            "description": "Webhook was set"
-//            "Webhook is already set"
-
         if ($this->getRequest()->getParams() !== []) {
-            $request = $this->getRequest();
-            $this->configWriter->save('tg_order_notification_bot/general/' . 'module_enabled', $request->getParam('enable'));
-            $this->configWriter->save('tg_order_notification_bot/general/' . 'access_token', $request->getParam('token'));
-            $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', $request->getParam('hook'));
-            $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 1);
-            try {
-                $this->client->get("https://api.telegram.org/bot" . $request->getParam('token') . "/setWebhook?url=" . $request->getParam('hook'));
-            }catch (\Exception $e){
-                $this->messageManager->addErrorMessage("Something went wrong wile setting hook. Please check hook url and token");
-                $this->messageManager->addErrorMessage($e->getMessage());
-                $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', '');
-                $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 0);
+            if ((bool)$this->getRequest()->getParam("delete")) {
+                $this->deleteWebhook();
+            } else {
+                $this->saveSettingsAndSetHook();
             }
-
-            $this->cacheManager->clean(['config']);
-            $this->messageManager->addSuccessMessage("New configuration was saved. Refresh page to see changes");
-        };
-
+        }
 
         $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
 
@@ -60,5 +41,49 @@ class Settings extends Action
             ->prepend(__('Edit settings telegram order notification bot'));
 
         return $resultPage;
+    }
+
+    /**
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function saveSettingsAndSetHook(): void
+    {
+        $request = $this->getRequest();
+        $this->configWriter->save('tg_order_notification_bot/general/' . 'module_enabled', $request->getParam('enable'));
+        $this->configWriter->save('tg_order_notification_bot/general/' . 'access_token', $request->getParam('token'));
+        $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', $request->getParam('hook'));
+        $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 1);
+        try {
+            $this->client->get("https://api.telegram.org/bot" . $request->getParam('token') . "/setWebhook?url=" . $request->getParam('hook'));
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage("Something went wrong wile setting hook. Please check hook url and token");
+            $this->messageManager->addErrorMessage($e->getMessage());
+            $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', '');
+            $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 0);
+        }
+
+        $this->cacheManager->clean(['config']);
+        $this->messageManager->addSuccessMessage("New configuration was saved. Refresh page to see changes");
+    }
+
+    /**
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function deleteWebhook(): void
+    {
+        $request = $this->getRequest();
+        try {
+            $this->client->get("https://api.telegram.org/bot" . $request->getParam('token') . "/deleteWebhook");
+            $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', '');
+            $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 0);
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage("Something went wrong wile setting hook. Please check  token");
+            $this->messageManager->addErrorMessage($e->getMessage());
+        }
+
+        $this->cacheManager->clean(['config']);
+        $this->messageManager->addSuccessMessage("Webhook was deleted. Refresh page to see changes");
     }
 }
