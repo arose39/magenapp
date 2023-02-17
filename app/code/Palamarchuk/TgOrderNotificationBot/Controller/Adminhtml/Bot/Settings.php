821460fe0116743d000e8546f0aa5d6116abf42e
Andrii Palamarchuk <a.palamarchuk@elogic.com.ua>
2023-02-17 15:04:00 +0200

    added set hook logic

--- app/code/Palamarchuk/TgOrderNotificationBot/Controller/Adminhtml/Bot/Settings.php
+++ app/code/Palamarchuk/TgOrderNotificationBot/Controller/Adminhtml/Bot/Settings.php
@@ -10,13 +10,15 @@ use Magento\Framework\App\Cache\Manager;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
+use GuzzleHttp\Client;
 
 class Settings extends Action
 {
     public function __construct(
-        Context $context,
-          private WriterInterface $configWriter,
-        private Manager $cacheManager
+        Context                 $context,
+        private WriterInterface $configWriter,
+        private Manager         $cacheManager,
+        private Client          $client
     )
     {
         parent::__construct($context);
@@ -25,12 +27,25 @@ class Settings extends Action
     public function execute(): ResultInterface
     {
 
-        if($this->getRequest()->getParams()!==[]){
+
+        // https://api.telegram.org/bot5882390745:AAF-lmwKIXukQMjIZhk_DeZ7V_UttQDurH4/setWebhook?url=https://magenapp.requestcatcher.com
+//            "description": "Webhook was set"
+//            "Webhook is already set"
+
+        if ($this->getRequest()->getParams() !== []) {
             $request = $this->getRequest();
             $this->configWriter->save('tg_order_notification_bot/general/' . 'module_enabled', $request->getParam('enable'));
             $this->configWriter->save('tg_order_notification_bot/general/' . 'access_token', $request->getParam('token'));
             $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', $request->getParam('hook'));
             $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 1);
+            try {
+                $this->client->get("https://api.telegram.org/bot" . $request->getParam('token') . "/setWebhook?url=" . $request->getParam('hook'));
+            }catch (\Exception $e){
+                $this->messageManager->addErrorMessage("Something went wrong wile setting hook. Please check hook url and token");
+                $this->messageManager->addErrorMessage($e->getMessage());
+                $this->configWriter->save('tg_order_notification_bot/general/' . 'hook', '');
+                $this->configWriter->save('tg_order_notification_bot/general/' . 'hook_is_set', 0);
+            }
 
             $this->cacheManager->clean(['config']);
             $this->messageManager->addSuccessMessage("New configuration was saved. Refresh page to see changes");
@@ -41,8 +56,8 @@ class Settings extends Action
 
         $resultPage->setActiveMenu('Palamarchuk_TgOrderNotificationBot::module');
         $resultPage->getConfig()
-                ->getTitle()
-                ->prepend(__('Edit settings telegram order notification bot'));
+            ->getTitle()
+            ->prepend(__('Edit settings telegram order notification bot'));
 
         return $resultPage;
     }
